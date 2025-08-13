dd1d0cff4f477e82ff83e3300ec3dd488e3e4652
f-qrm <qerimifjolla7@gmail.com>
2025-08-13 22:54:36 +0200

    feat: Count and display logs from the nginx collection in MongoDB.

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Count and display logs from the nginx collection in MongoDB."""
+from pymongo import MongoClient
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.log
+    collection = db.nginx
+
+    log_count = collection.count_documents({})
+    print(f"{log_count} logs")
+
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print(f"/tmethod {method}: {count}")
+
+    status_check = collection.count_documents(
+        {"method": "GET", "path": "/status"}
+        )
+    print(f"{status_check} status check")
