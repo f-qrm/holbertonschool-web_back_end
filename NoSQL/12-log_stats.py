975d82bb6a44379539b6f57e229ccd8a485160fd
f-qrm <qerimifjolla7@gmail.com>
2025-08-14 13:45:29 +0200

    fix: add E to DELETE and s to logs

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -3,17 +3,17 @@
 from pymongo import MongoClient
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    db = client.log
+    db = client.logs
     collection = db.nginx
 
     log_count = collection.count_documents({})
     print(f'{log_count} logs')
 
     print('Methods:')
-    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELET']
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
     for method in methods:
         count = collection.count_documents({'method': method})
-        print(f'/tmethod {method}: {count}')
+        print(f'\tmethod {method}: {count}')
 
     status_check = collection.count_documents(
         {'method': 'GET', 'path': '/status'}
