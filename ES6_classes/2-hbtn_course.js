d107521e9cafd7cad0b2534b9670aac73e095f75
f-qrm <qerimifjolla7@gmail.com>
2025-08-19 14:43:26 +0200

    fix: taking of the verification at the constructor

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,11 +1,8 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== "string") throw new TypeError("Name must be a string");
-    this._name = name;
-    if (typeof length !== "number") throw new TypeError("Length must be a number");
-    this._length = length;
-    if (!Array.isArray(students)) throw new TypeError('students must be an array')
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
   get name() {
     return this._name
