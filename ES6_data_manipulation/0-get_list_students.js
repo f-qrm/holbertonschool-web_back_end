13e8457ba0088ef14ec360a131758f76abccfc04
f-qrm <qerimifjolla7@gmail.com>
2025-08-25 10:18:42 +0200

    fix: change the dirname for the 3rd objet because in the task we had the wrong name

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,5 +1,5 @@
 export default function getListStudents() {
     const arr = [{id: 1, firstName: "Guillaume", location: "San Francisco"},
-        {id: 2, firstName: "James", location: "Columbia"}, {id: 3, firstName: "Serena", location: "San Francisco"}]
+        {id: 2, firstName: "James", location: "Columbia"}, {id: 5, firstName: "Serana", location: "San Francisco"}]
     return arr
 }
