807ddd02145346bd7a0c5ee1b44afbe5743b604e
f-qrm <qerimifjolla7@gmail.com>
2025-08-26 14:34:47 +0200

    feat(promise): implement handleProfileSignup using Promise.all with uploadPhoto and createUser

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,7 @@
+import {uploadPhoto, createUser} from "./utils.js";
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()]).then((result) => {
+    console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`)
+  })
+  .catch(() => new Error('Signup system offline'))
+}
