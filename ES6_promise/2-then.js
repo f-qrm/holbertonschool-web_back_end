1079c4f9d9710e568c0bb0035d4f228185506c68
f-qrm <qerimifjolla7@gmail.com>
2025-08-26 13:36:29 +0200

    fix: s instead of S

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -2,7 +2,7 @@ export default function handleResponseFromAPI(promise) {
   const success = true
   return new Promise((resolve, reject) => {
     if (success) {
-      resolve ( { 'status': 200, body: 'Success' } )
+      resolve ( { 'status': 200, body: 'success' } )
       console.log('Got a response from the API')
     }
     else {
