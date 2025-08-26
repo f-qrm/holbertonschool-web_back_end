7cea7cf99f2e3de7cc11f703b47ad420c009fb9d
f-qrm <qerimifjolla7@gmail.com>
2025-08-26 13:33:53 +0200

    feat: create handleResponseFromAPI function returning a promise

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,13 @@
+export default function handleResponseFromAPI(promise) {
+  const success = true
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve ( { 'status': 200, body: 'Success' } )
+      console.log('Got a response from the API')
+    }
+    else {
+      reject(new Error(''))
+      console.log('Got a response from the API')
+    }
+  })
+}
