6b2f2649f3fb1e5ead8f3bb7d3f3b3025da3193e
f-qrm <qerimifjolla7@gmail.com>
2025-08-26 10:01:46 +0200

    fix: handle empty or undefined startString in cleanSet

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') return '';
+  if (startString === '' || startString == null || startString == undefined) return '';
   const newList = []
   for (const value of set) {
     if (value.startsWith(startString)) {
