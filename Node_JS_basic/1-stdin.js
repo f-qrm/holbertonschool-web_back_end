0e05642b06a1b11c01fedc84be4f20a042f9a40d
f-qrm <qerimifjolla7@gmail.com>
2025-09-01 12:17:13 +0200

    style: ; at the end of consolelog to pass the check

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,9 @@
-process.stdin.setEncoding('utf8');
-
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('readable', () => {
-  var chunk = process.stdin.read();
+  const chunk = process.stdin.read();
   if (chunk !== null) {
-    process.stdout.write('Your name is: ' + chunk);
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
