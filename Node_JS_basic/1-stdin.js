dfa490a8bdaeb2d3715f9cd6780b0376c5219767
f-qrm <qerimifjolla7@gmail.com>
2025-09-01 11:28:40 +0200

    fix(stdin): correct code style and formatting for user input program

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,9 @@
 process.stdin.setEncoding('utf8');
 
-console.log('Welcome to Holberton School, what is your name?')
-process.stdin.on('data', function(input) {
-  console.log('Your name is: ' + input.trim());
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('data', (input) => {
+  console.log(`Your name is: ${input.trim()}`);
 });
-process.stdin.on('end', function() {
-  console.log('This important software is now closing')
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
 });
