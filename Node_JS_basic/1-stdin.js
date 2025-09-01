f93cac9e8b8e3f99d0f221433ff3d1b7b7d93e64
f-qrm <qerimifjolla7@gmail.com>
2025-09-01 11:53:21 +0200

    fix(stdin): handle terminal and pipe input correctly

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,18 @@
 process.stdin.setEncoding('utf8');
 
 console.log('Welcome to Holberton School, what is your name?');
+
+let inputHandled = false;
+
 process.stdin.on('data', (input) => {
-  console.log(`Your name is: ${input.trim()}`);
+  if (!inputHandled) {
+    console.log(`Your name is: ${input.trim()}`);
+    inputHandled = true;
+  }
 });
+
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  if (!process.stdin.isTTY) {
+    console.log('This important software is now closing');
+  }
 });
