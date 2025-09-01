bc04a0cad5e6bf4a9c3a291457ce6f5860af044b
f-qrm <qerimifjolla7@gmail.com>
2025-09-01 12:14:14 +0200

    feat(stdin): read user input using readable and display closing message

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,13 +2,13 @@ process.stdin.setEncoding('utf8');
 
 console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('data', (input) => {
-  console.log(`Your name is: ${input.trim()}`);
+process.stdin.on('readable', () => {
+  var chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write('Your name is: ' + chunk);
+  }
 });
 
 process.stdin.on('end', () => {
-  if (!process.stdin.isTTY) {
-    console.log('This important software is now closing');
-  }
+  console.log('This important software is now closing');
 });
-module.exports = null;
