63a4f770df22651716ba45d32714f8ee984fbec2
f-qrm <qerimifjolla7@gmail.com>
2025-09-03 11:16:38 +0200

    style: npx eslint

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -4,12 +4,12 @@ const countStudents = require('./3-read_file_async');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.set('Content-Type', 'text/plain');
+  res.type('text');
   res.send('Hello Holberton School!');
 });
 
 app.get('/students', async (req, res) => {
-  res.set('Content-Type', 'text/plain');
+  res.type('text');
 
   let output = '';
   const database = process.argv[2];
@@ -20,9 +20,10 @@ app.get('/students', async (req, res) => {
     await countStudents(database);
     output = output.trimEnd();
     res.send(`This is the list of our students\n${output}`);
+    return;
   } catch (err) {
     console.log = originalLog;
-    res.end('Cannot load the database');
+    res.send('Cannot load the database');
     return;
   } finally {
     console.log = originalLog;
